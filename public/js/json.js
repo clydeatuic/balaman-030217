736805cc8a688d1d44985fa7cccee0047481c0ae
clydeatuic <cbalamanatuic@gmail.com>
2017-03-02 20:36:37 +0800

    second commit

---
+++ public/js/json.js
@@ -0,0 +1,26 @@
+// console.log("Hello World");
+function x(){
+	var fn = document.querySelector('#fn').value;
+	var sn = document.getElementById('sn').value;
+	var sum = parseInt(fn) + parseInt(sn);
+	// console.log("The sum is " + sum);
+	document.querySelector('.result').innerHTML = sum;
+}
+
+$(function(){
+	$.ajax({
+		url:"http://www.omdbapi.com/?t=Game%20of%20Thrones"
+	}).done(function(data){
+		console.log(data);
+		let html = `
+			<a href="/">Home</a> |
+			<a href="/about">About</a> |
+			<a href="/json">JSON</a>
+			<h1>${data.Title}</h1>
+			<img src="${data.Poster}" />			
+		`;
+		document.querySelector('.app').innerHTML = html;
+
+	});
+
+})
